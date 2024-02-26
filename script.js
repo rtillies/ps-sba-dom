25ef594cf717c6ca645aa6f20dc886af45bb579a
Richard T <rtillies@gmail.com>
2024-02-26 02:00:56 -0500

    Refactor generateCardImage

--- script.js
+++ script.js
@@ -1,20 +1,28 @@
 const suits = ['spade', 'diamond', 'club', 'heart']
 const ranks = ['joker', 'ace', 'deuce', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'jack', 'queen', 'king']
 
-console.log(generateCardImage());
+for (let i = 0; i < 10; i++) {
+    console.log(generateCardImage());
+}
 
 
 function generateCardImage() {
     let randomRank = Math.floor(Math.random() * 14)
     let randomSuit = Math.floor(Math.random() * 4)
+    let rrr = ranks[randomRank]
 
     // console.log(randomRank, ranks[randomRank]); // 0-13
     // console.log(randomSuit, suits[randomSuit]); // 0-4
+    // console.log(randomRank, randomSuit);
 
-    let rr = randomRank < 10 ? '0' + randomRank : randomRank
-    let rrr = ranks[randomRank]
-    let srs = suits[randomSuit]
-
-    // console.log(`${srs}_${rr}_${rrr}.png`);
-    return `${srs}_${rr}_${rrr}.png`;
+    if (randomRank == 0) {
+        if (randomSuit % 2) // 1 or 3
+            return `${rrr}_big.png`;
+        else                // 0 or 2
+            return `${rrr}_little.png`;
+    } else {
+        let rr = randomRank < 10 ? '0' + randomRank : randomRank
+        let srs = suits[randomSuit]
+        return `${srs}_${rr}_${rrr}.png`;
+    }
 }
