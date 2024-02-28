1f29c1bb6a1bcf8af8a07f5e334724f5e0582470
Richard T <74796181+rtillies@users.noreply.github.com>
2024-02-28 16:10:39 -0500

    right-click format document

--- script.js
+++ script.js
@@ -1,42 +1,58 @@
-const playingCard = document.getElementById('playing-card')
-const cardHistory = document.querySelector('#history')
-const form = document.querySelector('form')
-const header = document.querySelector('.page-header')
-const newsletter = document.querySelector('#newsletter')
+const playingCard = document.getElementById("playing-card");
+const cardHistory = document.querySelector("#history");
+const form = document.querySelector("form");
+const header = document.querySelector(".page-header");
+const newsletter = document.querySelector("#newsletter");
 
-const player = form.elements['playerName'];
-const email = form.elements['playerEmail'];
-const checkbox = form.elements['referralCheck'];
-const code = form.elements['referralCode'];
-const submitBtn = form.elements['submit-btn'];
+const player = form.elements["playerName"];
+const email = form.elements["playerEmail"];
+const checkbox = form.elements["referralCheck"];
+const code = form.elements["referralCode"];
+const submitBtn = form.elements["submit-btn"];
 
-const suits = ['spade', 'diamond', 'club', 'heart']
-const ranks = ['joker', 'ace', 'deuce', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'jack', 'queen', 'king']
+const suits = ["spade", "diamond", "club", "heart"];
+const ranks = [
+  "joker",
+  "ace",
+  "deuce",
+  "three",
+  "four",
+  "five",
+  "six",
+  "seven",
+  "eight",
+  "nine",
+  "ten",
+  "jack",
+  "queen",
+  "king",
+];
 
 // Display browser information on the page
 displayBrowser();
 // updateNewsletterPanel();
 
 // Event listeners
-playingCard.addEventListener('click', changeCard)
-checkbox.addEventListener('click', toggleReferralCode)
-submitBtn.addEventListener('click', validate)
+playingCard.addEventListener("click", changeCard);
+checkbox.addEventListener("click", toggleReferralCode);
+submitBtn.addEventListener("click", validate);
 
 // Get browser info from window.navigator object
 function displayBrowser() {
-  const appVersion = window.navigator.appVersion.split(' ')
-  const version = 
-    (appVersion[appVersion.length - 1]) + ' | ' +
-    (appVersion[appVersion.length - 2])
+  const appVersion = window.navigator.appVersion.split(" ");
+  const version =
+    appVersion[appVersion.length - 1] +
+    " | " +
+    appVersion[appVersion.length - 2];
 
-  const browser = document.createElement('p')
+  const browser = document.createElement("p");
   browser.innerHTML = `
     <strong>Browser:</strong> 
     ${window.navigator.appCodeName} | 
     ${window.navigator.appName} | 
-    ${version}`
-  
-  header.append(browser)
+    ${version}`;
+
+  header.append(browser);
 }
 
 // Validate full form
@@ -56,13 +72,12 @@ function validate(evt) {
 // Hide form
 // Update card title and text
 function updateNewsletterPanel(name, email, code) {
-  const headline = newsletter.firstElementChild
-  const text = newsletter.getElementsByClassName('newsletter-text')[0]
-  const form = newsletter.getElementsByTagName('form')[0]
+  const headline = newsletter.firstElementChild;
+  const text = newsletter.getElementsByClassName("newsletter-text")[0];
+  const form = newsletter.getElementsByTagName("form")[0];
 
-  
-  form.hidden = true
-  headline.textContent += ' Sent!'
+  form.hidden = true;
+  headline.textContent += " Sent!";
 
   text.innerHTML = `
   <h5>SUCCESS!</h5>
@@ -76,8 +91,8 @@ function updateNewsletterPanel(name, email, code) {
   </p>
 
   <p>
-  Referral Code: <b>${code || 'n/a'}</b>
-  </p>`
+  Referral Code: <b>${code || "n/a"}</b>
+  </p>`;
 }
 
 // Validate name field
@@ -87,7 +102,7 @@ function validateName() {
     player.focus();
     return false;
   } else {
-    const nameParts = player.value.split(' ') 
+    const nameParts = player.value.split(" ");
     if (nameParts.length < 2) {
       window.alert("Please enter your first and last name.");
       player.focus();
@@ -114,8 +129,8 @@ function validateEmail() {
 
 // Validate referral code field
 // Disabled until checkbox is checked
-  // Cannot be blank
-  // Must contain only digits
+// Cannot be blank
+// Must contain only digits
 function validateCode() {
   if (checkbox.checked && code.value === "") {
     window.alert("Please provide a referral code or uncheck the box.");
@@ -132,80 +147,81 @@ function validateCode() {
 
 // Change playing card in browser
 function changeCard(event) {
-    let card = getRandomCard()
-    let words = parseCardToWords(card)    
-    
-    playingCard.setAttribute('src', `cards/${card}`)
-    playingCard.setAttribute('alt', words)
-    addHistory(words)
+  let card = getRandomCard();
+  let words = parseCardToWords(card);
+
+  playingCard.setAttribute("src", `cards/${card}`);
+  playingCard.setAttribute("alt", words);
+  addHistory(words);
 }
 
 // Add card details to history panel
 function addHistory(card) {
-    // remove active class from current active item
-    const activeItem = document.querySelector('.list-group-item.active')
-    activeItem.classList.remove('active')
+  // remove active class from current active item
+  const activeItem = document.querySelector(".list-group-item.active");
+  activeItem.classList.remove("active");
 
-    // create new list item
-    const li = document.createElement('li')
-    li.textContent = card
-    li.classList.add('list-group-item')
-    li.classList.add('active')
+  // create new list item
+  const li = document.createElement("li");
+  li.textContent = card;
+  li.classList.add("list-group-item");
+  li.classList.add("active");
 
-    // add new item to add to history
-    cardHistory.prepend(li)
+  // add new item to add to history
+  cardHistory.prepend(li);
 
-    // limit history list to 10 items
-    if (cardHistory.children.length > 10) {
-        let lastChild = cardHistory.lastElementChild;
-        console.log(lastChild);
-        cardHistory.removeChild(lastChild)
-    }
+  // limit history list to 10 items
+  if (cardHistory.children.length > 10) {
+    let lastChild = cardHistory.lastElementChild;
+    console.log(lastChild);
+    cardHistory.removeChild(lastChild);
+  }
 }
 
 // enable/disable referral code based on checkbox
 function toggleReferralCode() {
   if (checkbox.checked) {
-    code.disabled = false
+    code.disabled = false;
   } else {
-    code.disabled = true
-    code.value = ''
+    code.disabled = true;
+    code.value = "";
   }
 }
 
 // get random playing card
 function getRandomCard() {
-    let randomRank = Math.floor(Math.random() * 14)
-    let randomSuit = Math.floor(Math.random() * 4)
-    let rrr = ranks[randomRank]
+  let randomRank = Math.floor(Math.random() * 14);
+  let randomSuit = Math.floor(Math.random() * 4);
+  let rrr = ranks[randomRank];
 
-    if (randomRank == 0) {
-        if (randomSuit % 2) // 1 or 3
-            return `${rrr}_big.png`;
-        else                // 0 or 2
-            return `${rrr}_little.png`;
-    } else {
-        let rr = randomRank < 10 ? '0' + randomRank : randomRank
-        let srs = suits[randomSuit]
-        return `${srs}_${rr}_${rrr}.png`;
-    }
+  if (randomRank == 0) {
+    if (randomSuit % 2)
+      // 1 or 3
+      return `${rrr}_big.png`;
+    // 0 or 2
+    else return `${rrr}_little.png`;
+  } else {
+    let rr = randomRank < 10 ? "0" + randomRank : randomRank;
+    let srs = suits[randomSuit];
+    return `${srs}_${rr}_${rrr}.png`;
+  }
 }
 
 // describe playing card in words
 // e.g. "deuce of spades", "king of diamonds"
 function parseCardToWords(image) {
-    let parts = image.split('_')
-    let lastPart = parts[parts.length - 1]
-    let rank = lastPart.split('.')[0]
-    parts[parts.length - 1] = rank
-    
-    if (parts[0] == 'joker') {
-        if (parts[1] == 'big') {
-            return 'Big Joker'
-        } else {
-            return 'Little Joker'
-        }
+  let parts = image.split("_");
+  let lastPart = parts[parts.length - 1];
+  let rank = lastPart.split(".")[0];
+  parts[parts.length - 1] = rank;
+
+  if (parts[0] == "joker") {
+    if (parts[1] == "big") {
+      return "Big Joker";
     } else {
-        return `${parts[2]} of ${parts[0]}s`
+      return "Little Joker";
     }
+  } else {
+    return `${parts[2]} of ${parts[0]}s`;
+  }
 }
